24f55dded3b3e880e338e4e410d0dcc1ba1465df
Mabry Cervin <Mabry.Cervin@inin.com>
2017-06-14 11:37:41 -0400

    Fixed function

--- jobs/bitbucket-event/build_status_updated.js
+++ jobs/bitbucket-event/build_status_updated.js
@@ -1,7 +1,5 @@
 const build_status = require('./build_status_created')
 
 module.exports = async function(data) {
-    return function () {
-              build_status(data)
-           }
+    return build_status(data)
 }
